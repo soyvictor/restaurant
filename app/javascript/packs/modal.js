b693f29d57d11bf7efd690d45280232683834437
Victor Chiang <vgchiang@gmail.com>
2021-01-26 13:36:17 -0600

    added anchors to navbars

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -2,7 +2,6 @@
 const foodItem = document.querySelector('.testItem');
 const modal = document.querySelector('.modal');
 
-
 function openModal() {
     console.info('Opening Modal...');
     // First check if the modal is already open
@@ -30,6 +29,6 @@ function openModal() {
   }
 
 
-// These our event listeners
+// These are our event listeners
 foodItem.addEventListener('click', openModal);
 modal.addEventListener('click', handleClickOutside);
