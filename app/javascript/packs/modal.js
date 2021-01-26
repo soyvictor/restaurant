ef05a2c4765f9788ce4ced2fc38bb32a9c3ac1bb
Victor Chiang <vgchiang@gmail.com>
2021-01-25 22:44:21 -0600

    added modal

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,10 +1,19 @@
 // Selectors
 const foodItem = document.querySelector('.testItem');
+const modal = document.querySelector('.modal');
 
 
 function openModal() {
-  console.log('modal opening...');
-}
+    console.info('Opening Modal...');
+    // First check if the modal is already open
+    if(modal.matches('.open')) {
+      console.info('Modal already open');
+      return; //stop the function from opening
+    }
+    modal.classList.add('open');
+
+    // Event listeners to be bound when we open the modal.
+  }
 
 
 foodItem.addEventListener('click', openModal);
