047ce4b0954f6ccda7879948733d708e6aa92225
Victor Chiang <vgchiang@gmail.com>
2021-01-27 15:59:09 -0600

    added seed file, styled login pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_233319) do
+ActiveRecord::Schema.define(version: 2021_01_27_215148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +46,7 @@ ActiveRecord::Schema.define(version: 2020_12_10_233319) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Item.destroy_all if Rails.env.development?
+Category.destroy_all if Rails.env.development?
+
+puts 'Creating categories...'
+torta = Category.create!(name: 'torta', user: User.first)
+bebida = Category.create!(name: 'bebida', user: User.first)
+
+puts 'Creating items...'
+Item.create!(category: torta, name: 'torta de pierna', description: 'Orden de 4 tacos en tortillas de maíz', user: User.first)
+Item.create!(category: bebida, name: 'coca cola', description: 'A popular and nice refreshing soda', user: User.first)
+
+puts 'Finished!'
