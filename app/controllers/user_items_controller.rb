9f7001f1bcf0eb1687b677a98b940354538ed3d4
Victor Chiang <vgchiang@gmail.com>
2021-01-28 16:59:39 -0600

    added create item link on nav

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,5 +1,23 @@
 class UserItemsController < ApplicationController
   def index
+    # @cart = UserItem.all
     @cart = UserItem.all
+  end
+
+  def new
+    @user_item = UserItem.new
+  end
+
+  def create
+    @user_item = UserItem.new
+    item_id
+    special_instructions
+    quantity
+    @user = current_user
+    @user_item.save
+    redirect_to user_items_path
+  end
+
+  def edit
   end
 end
