b3fc2902fba25b9c08b8b8e9335fe180a8a50afa
Victor Chiang <vgchiang@gmail.com>
2021-02-10 20:14:27 -0600

    styled menu category headings

---
+++ app/javascript/packs/menu_categories.js
@@ -0,0 +1,46 @@
+// Declarations
+
+const categoryNames = document.querySelectorAll('.category-name');
+let categorySelected = undefined;
+
+// Functions
+
+function addHighlight(e) {
+  categoryNames.forEach((name) => {
+    name.classList.remove('categoryNameHighlight');
+  });
+  e.target.classList.add('categoryNameHighlight');
+  categorySelected = e.target
+}
+
+
+function underline(e) {
+  console.log('highlighting!');
+  categoryNames.forEach((name) => {
+    name.classList.remove('categoryNameHighlight');
+  });
+
+  e.target.classList.add('categoryNameHighlight');
+}
+
+function underlineCategorySelected() {
+  categoryNames.forEach((name) => {
+    name.classList.remove('categoryNameHighlight');
+  });
+  categorySelected.classList.add('categoryNameHighlight');
+}
+
+
+// Event Listeners
+
+categoryNames.forEach((name) => {
+  name.addEventListener('click', addHighlight);
+});
+
+categoryNames.forEach((name) => {
+  name.addEventListener('mouseover', underline);
+});
+
+categoryNames.forEach((name) => {
+  name.addEventListener('mouseout', underlineCategorySelected);
+});
