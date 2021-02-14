803a487d191fbef86892d0533f5365af95faa438
Victor Chiang <vgchiang@gmail.com>
2021-02-14 00:38:11 -0600

    add menu scroll and anchor functionality

--- app/javascript/packs/menu_categories.js
+++ app/javascript/packs/menu_categories.js
@@ -3,44 +3,45 @@ const categoryNameFirst = document.querySelector('.category-name');
 const categoryNames = document.querySelectorAll('.category-name');
 let categorySelected = categoryNames[0];  // Not sure if this is correct syntax
 const intersectionWatch = document.querySelectorAll('.intersection');
+const menuBox = document.querySelector('.menu-items')
 
-const menuHeading = document.querySelector('#order');
+// const menuHeading = document.querySelector('#order');
 // Functions
 
-function conlog() {
-  console.log(menuHeading.getBoundingClientRect().top);
+// function conlog() {
+//   console.log(menuHeading.getBoundingClientRect().top);
+// }
+
+// window.addEventListener('scroll', conlog);
+
+function addHighlightIntersection(element) {
+  categoryNames.forEach((name) => {
+    name.classList.remove('categoryNameHighlight');
+  });
+  categoryNames.forEach((name) => {
+    if (name.innerText === element) {
+      name.classList.add('categoryNameHighlight');
+      categorySelected = name;
+    }
+  });
 }
 
-window.addEventListener('scroll', conlog);
 
-// function addHighlightIntersection(element) {
-//   categoryNames.forEach((name) => {
-//     name.classList.remove('categoryNameHighlight');
-//   });
-//   categoryNames.forEach((name) => {
-//     if (name.innerText === element) {
-//       name.classList.add('categoryNameHighlight');
-//       categorySelected = name;
-//     }
-//   });
-// }
+function obCallBack(payload) {
+  if(payload[0].intersectionRatio === 1) {
+    console.log('Changed Categories');
+    const element = payload[0].target.id;
+    console.log(element);
+    addHighlightIntersection(element);
+  }
 
+}
 
-// function obCallBack(payload) {
-//   if(payload[0].intersectionRatio === 1) {
-//     console.log('Changed Categories');
-//     const element = payload[0].target.id;
-//     console.log(element);
-//     addHighlightIntersection(element);
-//   }
-
-// }
-
-// const ob = new IntersectionObserver(obCallBack, { threshold: 1 });
-
-// intersectionWatch.forEach((category) => {
-//   ob.observe(category)
-// });
+const ob = new IntersectionObserver(obCallBack, { threshold: 1, rootMargin: "-40% 0px -40% 0px" });
+// 150 and 400
+intersectionWatch.forEach((category) => {
+  ob.observe(category)
+});
 
 
 
