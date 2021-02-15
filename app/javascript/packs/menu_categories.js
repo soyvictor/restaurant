7b2e105b0206f8bb534f8e5c537890beb59a74a0
Victor Chiang <vgchiang@gmail.com>
2021-02-15 15:19:23 -0600

    add js for menu highlighting

--- app/javascript/packs/menu_categories.js
+++ app/javascript/packs/menu_categories.js
@@ -1,57 +1,17 @@
 // Declarations
 const categoryNameFirst = document.querySelector('.category-name');
 const categoryNames = document.querySelectorAll('.category-name');
-let categorySelected = categoryNames[0];  // Not sure if this is correct syntax
-const intersectionWatch = document.querySelectorAll('.intersection');
-const menuBox = document.querySelector('.menu-items')
-
-// const menuHeading = document.querySelector('#order');
-// Functions
-
-// function conlog() {
-//   console.log(menuHeading.getBoundingClientRect().top);
-// }
-
-// window.addEventListener('scroll', conlog);
-
-function addHighlightIntersection(element) {
-  categoryNames.forEach((name) => {
-    name.classList.remove('categoryNameHighlight');
-  });
-  categoryNames.forEach((name) => {
-    if (name.innerText === element) {
-      name.classList.add('categoryNameHighlight');
-      categorySelected = name;
-    }
-  });
-}
-
-
-function obCallBack(payload) {
-  if(payload[0].intersectionRatio === 1) {
-    console.log('Changed Categories');
-    const element = payload[0].target.id;
-    console.log(element);
-    addHighlightIntersection(element);
-  }
-
-}
-
-const ob = new IntersectionObserver(obCallBack, { threshold: 1, rootMargin: "-40% 0px -40% 0px" });
-// 150 and 400
-intersectionWatch.forEach((category) => {
-  ob.observe(category)
-});
-
-
-
+let selectedCategory = categoryNames[0];
+const menuBox = document.querySelector('.menu-items');
 
 function addHighlight(e) {
+  selectedCategory = e.target;
+  console.log(selectedCategory);
   categoryNames.forEach((name) => {
     name.classList.remove('categoryNameHighlight');
   });
   e.target.classList.add('categoryNameHighlight');
-  categorySelected = e.target;
+
 }
 
 
@@ -59,15 +19,15 @@ function underline(e) {
   categoryNames.forEach((name) => {
     name.classList.remove('categoryNameHighlight');
   });
-
   e.target.classList.add('categoryNameHighlight');
 }
 
 function underlineCategorySelected() {
+  console.log(selectedCategory);
   categoryNames.forEach((name) => {
     name.classList.remove('categoryNameHighlight');
   });
-  categorySelected.classList.add('categoryNameHighlight');
+  selectedCategory.classList.add('categoryNameHighlight');
 }
 
 
