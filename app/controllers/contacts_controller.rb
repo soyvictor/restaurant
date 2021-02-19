e996b16ed3335951c62ccfdc76fec765d6b026d7
Victor Chiang <vgchiang@gmail.com>
2021-02-18 23:18:53 -0600

    styled the second checkout page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,28 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
-  def crate
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.user = current_user
+
+    if @contact.save
+      return
+    else
+      render :payments
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :phone)
   end
 end
