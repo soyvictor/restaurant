d7087f28e4720cd8416a835182aecfbca1f55b43
Victor Chiang <vgchiang@gmail.com>
2021-02-20 00:31:01 -0600

    ...

--- app/javascript/packs/checkout_summary.js
+++ app/javascript/packs/checkout_summary.js
@@ -1,12 +1,19 @@
 const orderType = document.getElementById('order-type-form');
 const orderAmount = document.querySelector('.order-amount');
-console.log(orderAmount);
+const addressFieldset = document.querySelector('.address-fieldset');
+const deliveryInfoHeading = document.querySelector('.delivery-info-heading');
 
 function updateCheckoutPrice(e) {
   if (e.target.value === 'pickup') {
     orderAmount.innerText = orderAmount.dataset.amount - orderAmount.dataset.delivery;
+    addressFieldset.disabled = true;
+    addressFieldset.classList.add('pickup');
+    deliveryInfoHeading.innerText = "Pickup Location"
   } else {
-    orderAmount.innerText = orderAmount.dataset.amount;
+    orderAmount.innerText = orderAmount.dataset.amount + orderAmount.dataset.delivery;
+    addressFieldset.classList.remove('pickup');
+    deliveryInfoHeading.innerText = "Delivery Info"
+    addressFieldset.disabled = false;
   }
 }
 
