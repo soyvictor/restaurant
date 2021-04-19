f767db632a5867ebed7a519d615832844d3a7178
Victor Chiang <vgchiang@gmail.com>
2021-04-19 15:51:00 -0700

    improved the intersection observer rootmargin area

--- app/javascript/packs/menu_categories.js
+++ app/javascript/packs/menu_categories.js
@@ -4,46 +4,87 @@ const categoryNames = document.querySelectorAll('.category-name');
 let selectedCategory = categoryNames[0];
 const menuBox = document.querySelector('.menu-items');
 
-function addHighlight(e) {
-  selectedCategory = e.target;
-  console.log(selectedCategory);
+//begin intersection observer code
+
+const intersectionWatch = document.querySelectorAll('.intersection');
+let options = {
+  root: null,
+  rootMargin: "-50% 0px -50% 0px",
+  // rootMargin: "-200px 0px 100px 0px",
+  // threshold: 0.01
+}
+function addHighlightIntersection(element) {
   categoryNames.forEach((name) => {
     name.classList.remove('categoryNameHighlight');
   });
-  e.target.classList.add('categoryNameHighlight');
-
-}
-
-
-function underline(e) {
   categoryNames.forEach((name) => {
-    name.classList.remove('categoryNameHighlight');
+    if (name.innerText === element) {
+      name.classList.add('categoryNameHighlight');
+      categorySelected = name;
+    }
   });
-  e.target.classList.add('categoryNameHighlight');
 }
 
-function underlineCategorySelected() {
-  console.log(selectedCategory);
-  categoryNames.forEach((name) => {
-    name.classList.remove('categoryNameHighlight');
-  });
-  selectedCategory.classList.add('categoryNameHighlight');
+
+function obCallBack(entries) {
+  entries.forEach(entry => {
+    if( entry.isIntersecting) {
+      // console.log("intersecting");
+      const element = entry.target.classList[0];
+      addHighlightIntersection(element);
+    }
+  })
 }
+
+let observer = new IntersectionObserver(obCallBack, options);
+
+intersectionWatch.forEach((category) => {
+  observer.observe(category)
+});
+
+// function addHighlight(e) {
+//   selectedCategory = e.target;
+//   console.log(selectedCategory);
+//   categoryNames.forEach((name) => {
+//      name.classList.remove('categoryNameHighlight');
+//   });
+//   selectedCategory.classList.add('categoryNameHighlight');
+// }
+
+
+// end intersection observer code
+
+
+// function underline(e) {
+//   categoryNames.forEach((name) => {
+//     name.classList.remove('categoryNameHighlight');
+//   });
+//   e.target.classList.add('categoryNameHighlight');
+// }
+
+// function underlineCategorySelected() {
+//   console.log(selectedCategory);
+//   categoryNames.forEach((name) => {
+//     name.classList.remove('categoryNameHighlight');
+//   });
+//   selectedCategory.classList.add('categoryNameHighlight');
+// }
 
 
 // Event Listeners
 
-categoryNames.forEach((name) => {
-  name.addEventListener('click', addHighlight);
-});
+// categoryNames.forEach((element) => {
+//   console.log(element);
+//   element.addEventListener('click', addHighlight);
+// });
 
-categoryNames.forEach((name) => {
-  name.addEventListener('mouseover', underline);
-});
+// categoryNames.forEach((name) => {
+//   name.addEventListener('mouseover', underline);
+// });
 
-categoryNames.forEach((name) => {
-  name.addEventListener('mouseout', underlineCategorySelected);
-});
+// categoryNames.forEach((name) => {
+//   name.addEventListener('mouseout', underlineCategorySelected);
+// });
 
 // Initializer
 categoryNameFirst.classList.add('categoryNameHighlight');
