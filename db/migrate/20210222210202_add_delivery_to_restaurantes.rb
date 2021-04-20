9890e85d52da098da31748ce710a99a63117ec50
Victor Chiang <vgchiang@gmail.com>
2021-04-19 18:52:16 -0700

    commented out offending migration

--- db/migrate/20210222210202_add_delivery_to_restaurantes.rb
+++ db/migrate/20210222210202_add_delivery_to_restaurantes.rb
@@ -1,5 +1,5 @@
 class AddDeliveryToRestaurantes < ActiveRecord::Migration[6.0]
-  def change
-    add_monetize :restaurantes, :delivery_fee, currency: { present: false }
-  end
+  # def change
+  #   add_monetize :restaurantes, :delivery_fee, currency: { present: false }
+  # end
 end
